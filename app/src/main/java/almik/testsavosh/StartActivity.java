9115224390e2e8abb089f8a509600381b3e195c3
alex <alexandr.pr29@gmail.com>
2016-12-08 00:30:28 +0200

    --second

--- app/src/main/java/almik/testsavosh/StartActivity.java
+++ app/src/main/java/almik/testsavosh/StartActivity.java
@@ -1,21 +1,19 @@
 package almik.testsavosh;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import almik.testsavosh.utill.restUtill;
 
 public class StartActivity extends AppCompatActivity {
 
-    private GoogleApiClient client;
 
 
 
@@ -24,56 +22,46 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView titleText = (TextView)findViewById(R.id.textView);
-        TextView bodyText = (TextView)findViewById(R.id.textView2);
+
         restUtill restUtill=new restUtill();
         restUtill.retro();
-        // [START handle_data_extras]
+
         if (getIntent().getExtras() != null) {
-            Intent intent = getIntent();
-            String title = intent.getStringExtra("title");
-            String body = intent.getStringExtra("body");
-            titleText.setText(title);
-            bodyText.setText(body);
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d("test1", "Key: " + key + " Value: " + value);
+            }
+      }
+
+    Button subscribeButton = (Button) findViewById(R.id.FirebaseMessaging1);
+    subscribeButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // [START subscribe_topics]
+            FirebaseMessaging.getInstance().subscribeToTopic("news");
+            // [END subscribe_topics]
+
+            // Log and toast
+            String msg = getString(R.string.msg_subscribed);
+            Log.d("test1", msg);
+            Toast.makeText(StartActivity.this, msg, Toast.LENGTH_SHORT).show();
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
+    });
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Start Page")
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
+    Button logTokenButton = (Button) findViewById(R.id.FirebaseInstanceId1);
+    logTokenButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Get token
+            String token = FirebaseInstanceId.getInstance().getToken();
 
-    }
+            // Log and toast
+            String msg = getString(R.string.msg_token_fmt, token);
+            Log.d("test1", msg);
+            Toast.makeText(StartActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+    });
+}
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
